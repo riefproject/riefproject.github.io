d4e759266b8554328bfcdb9175b93e552d8bdb2b
riefproject <riefedu@gmail.com>
2025-12-28 06:14:55 +0700

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: true,
     }),
+    sitemap(),
   ],
 });
